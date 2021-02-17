d63ec50d0cd21852fe371989f2733013a5de2c83
jasminelok <jasminellok@gmail.com>
2021-02-17 00:26:04 -0500

    Update book model and tests: .q .exit quit

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,11 +9,12 @@ RSpec.describe Author, type: :model do
 
   describe "validations" do
     it {should validate_presence_of(:full_name)} 
+    subject { Author.new(full_name: "jk rowling") }
     it {should validate_uniqueness_of(:full_name)} 
   end 
 
   describe "associations" do
-    it {should have_many(:books).through(:)}
+    it {should have_many(:books).through(:book_authors)}
   end 
 
   # describe "class methods" do 
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
+#to run bundle exec rspec spec/models/book_spec.rb
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it {should validate_presence_of(:title)} 
+    subject { Book.new(title: "harry potter #1") }
+    it {should validate_uniqueness_of(:title)} 
+  end 
+
+  describe "associations" do
+    it {should have_many(:authors).through(:book_authors)}
+  end 
 end
