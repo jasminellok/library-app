9359aad1ab0a52b691d9fbbb02dc6c2924026aa3
jasminelok <jasminellok@gmail.com>
2021-02-17 22:27:51 -0500

    Update BookAuthor model with validation and associations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,5 @@
 class Author < ApplicationRecord
     validates :full_name, presence: true, uniqueness: true
+
+    
 end
--- app/models/book_author.rb
+++ app/models/book_author.rb
@@ -1,3 +1,11 @@
 class BookAuthor < ApplicationRecord
-    
+    validates :book_id, :author_id, presence:true
+
+    belongs_to :book,
+        foreign_key: :book_id,
+        class_name: :Book
+
+    belongs_to :author,
+        foreign_key: :author_id,
+        class_name: :Author
 end
