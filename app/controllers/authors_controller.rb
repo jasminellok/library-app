7beaa8286710367513f992ace46e6c443dd828ea
jasminelok <jasminellok@gmail.com>
2021-02-18 01:42:24 -0500

    Update controllers

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,12 @@
 class AuthorsController < ApplicationController
+    def show
+        @author = Author.find_by(id: params[:id])
+        render "authors/show"
+    end
+    
+    def index
+        @authors = Author.all.includes(:books)
+        render "authors/index"
+    end
+
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,11 @@
 class BooksController < ApplicationController
+     def show
+        @book = Book.find_by(id: params[:id])
+        render "books/show"
+    end
+    
+    def index
+        @books = Books.all.includes(:authors)
+        render "books/index"
+    end
 end
