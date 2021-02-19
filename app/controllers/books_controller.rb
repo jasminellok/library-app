318ed410816d51b3678e1c3375c4e64fbb2820e6
jasminelok <jasminellok@gmail.com>
2021-02-19 02:09:23 -0500

    Setup backend to mark/unmark a book as a duplicate of another

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,18 @@ class BooksController < ApplicationController
         @books = Book.all.includes(:authors)
         render "books/index"
     end
+
+    def update
+        @book = Book.find_by(id: params[:id])
+        if @book && @book.update(book_params)
+            render "books/show"
+        else
+            render json: @book.errors.full_messages
+        end
+    end
+
+    protected
+    def book_params
+        params.require(:book).permit(:parent_id)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
   resources :authors, only: [:index, :show]
-  resources :books, only: [:index, :show]
-  resources :book_authors, only: [:create, :destroy]
+  resources :books, only: [:index, :show, :update]
+
 end
