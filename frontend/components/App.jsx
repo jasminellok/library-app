11b9359ab3b7ea3b057a9835ea050050fd615a58
jasminelok <jasminellok@gmail.com>
2021-02-18 23:45:22 -0500

    Update book show component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Splash from "./splash";
 import AuthorShowContainer from "./authors/author_show_container";
+import BookShowContainer from "./books/book_show_container";
 
 const App = () => (
   <div>
     <Switch>
         <Route exact path="/" component={Splash} />
         <Route exact path="/authors/:authorId" component={AuthorShowContainer} />
+        <Route exact path="/books/:bookId" component={BookShowContainer} />
     </Switch>
   </div>
 );
---
+++ frontend/components/books/book_show_container.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchBook} from '../../actions/book_actions';
+
+class BookShow extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+   
+    componentDidMount() {
+        this.props.fetchBook(this.props.match.params.bookId) 
+    }
+
+    render() { 
+        if (!this.props.book) return null;
+        let authors = this.props.book.authorId
+        let bookAuthors;
+
+        if (authors){
+            bookAuthors = authors.map((author) => {
+                return (<li key={`book-show-author${author.id}`}>{author.full_name}</li>)
+            });  
+        } else {
+            bookAuthors = null
+        }
+
+        return (<div>
+            <Link to={`/`}>Go Back to Home</Link>
+            
+            <h1>Book: {this.props.book.title}</h1>
+
+            <h2>Book written by:</h2>
+            <ul> 
+                {bookAuthors}
+            </ul>
+        </div>)
+    }
+}
+
+const mstp = (state, ownProps) => {
+    const book = state.entities.books;
+    const bookId = ownProps.match.params.bookId;
+    return {
+        book: book[bookId]
+    }
+}
+
+const mdtp = dispatch => {
+    return {
+        fetchBook: (id) => dispatch(fetchBook(id))
+    }
+}
+
+export default connect(mstp, mdtp)(BookShow);
