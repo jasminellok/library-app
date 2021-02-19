48a06749b78307b61206c16e85a26cc467740e14
jasminelok <jasminellok@gmail.com>
2021-02-18 20:21:06 -0500

    Setup frontend

--- frontend/library.jsx
+++ frontend/library.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";;
-import configureStore from "./store/store";
-import Root from "./components/root";
+import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
-    let store = configureStore();
-
     const root = document.getElementById("root");
-    ReactDOM.render(<Root store={store} />, root);
+    ReactDOM.render(<h1>hello world</h1>, root);
     
 });
